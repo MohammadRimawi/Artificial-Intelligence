2e433b4763792753c04aa94c4ea38803d584bd9c
Mohammad Rimawi <mohrimawiz@gmail.com>
2021-04-11 15:40:29 +0300

    fixing fitness bug, needs better fitness selection

--- grid heuristic.py
+++ grid heuristic.py
@@ -1,4 +1,4 @@
-import operator
+import operator,random
 goal = [
     [1,2,3],
     [8,0,4],
@@ -11,7 +11,7 @@ m = 3
 class Grid:
     counter = 0
     def __init__(self,grid,parent=[-1,-1]):
-        self.id=Grid.counter
+        self.id = Grid.counter
         Grid.counter+=1
         self.parent = parent
         self.grid = grid
@@ -19,15 +19,17 @@ class Grid:
         self.fitness = 0
 
     def calculate_fitness(self,):
-        # fitness = 0
+        fitness = 0
         # output(self)
+        self.fitness = 0
         for i in range(len(self.grid)):
             for j in range(len(self.grid[i])):
                 if self.grid[i][j]==0:
                     self.empty_at = [i,j]
-                g.fitness += abs(distance[self.grid[i][j]][0] - i) + abs(distance[self.grid[i][j]][1] - j)
+                fitness += abs(distance[self.grid[i][j]][0] - i) + abs(distance[self.grid[i][j]][1] - j)
 
-        return g.fitness
+        self.fitness = fitness
+        return self.fitness
 
 
 distance = [
@@ -43,7 +45,7 @@ def initialize():
 def output(g):
     for i in g.grid:
         print(i)
-    print(g.fitness,g.id)
+    print("fitness:",g.fitness,"\nID:",g.id,"\nparent:",g.parent,"\nempty at:",g.empty_at)
     print("")
         
 def output_grid(g):
@@ -53,72 +55,84 @@ def output_grid(g):
 
 def get_first(queue):
     queue.sort(key=operator.attrgetter("fitness"))
+    for i in range(min(5,len(queue))):
+        output(queue[i])
+        # print("***********************************")
     first = []
     for i in range(len(queue)):
-        if queue[i].fitness==queue[0].fitness:
+        if queue[i].fitness==queue[0].fitness or i<100:
             first.append(queue[i])
-            output(queue[i])
+            # output(queue[i])
+    # print(first)
+    # return queue
     return first
 
-def BFS(g):
-    parent = g.parent
+def BFS(src):
+    parent = src.parent
 
     steps = [[0,1],[1,0],[0,-1],[-1,0]]
 
-    queue = []
-    queue.append(g)
+    current_queue = []
+    next_queue = []
 
-    while( g.grid != goal and len(queue)>0):
+    next_queue.append(src)
+
+    while( src.grid != goal):
+        random.shuffle(steps)
+        print(steps)
+        current_queue=[]
+        current_queue = get_first(next_queue)
+        next_queue = []
         
-        queue = get_first(queue)
-        print(queue)
-        for s in steps:
-            if(g.empty_at[0]+s[0] in range(n) and g.empty_at[1]+s[1] in range(m)):
-                cgrid = [ 
-                        [None,None,None],
-                        [None,None,None],
-                        [None,None,None],
-                        ] 
-                for i in range(len(cgrid)):
-                    for j in range(len(cgrid[i])):
-                        cgrid[i][j]=g.grid[i][j]
-                        pass
-                cgrid[g.empty_at[0]+s[0]][g.empty_at[1]+s[1]] , cgrid[g.empty_at[0]][g.empty_at[1]] = cgrid[g.empty_at[0]][g.empty_at[1]],cgrid[g.empty_at[0]+s[0]][g.empty_at[1]+s[1]]            
+        for i in range(len(current_queue)):
+            src = current_queue[i]
+            for s in steps:
+                if(src.empty_at[0]+s[0] in range(n) and src.empty_at[1]+s[1] in range(m) and [src.empty_at[0]+s[0],src.empty_at[1]+s[1]]!=src.parent):
+                    cgrid = [ 
+                            [None,None,None],
+                            [None,None,None],
+                            [None,None,None],
+                            ] 
+                    for i in range(len(cgrid)):
+                        for j in range(len(cgrid[i])):
+                            cgrid[i][j]=src.grid[i][j]
+                            pass
+                    cgrid[src.empty_at[0]+s[0]][src.empty_at[1]+s[1]] , cgrid[src.empty_at[0]][src.empty_at[1]] = cgrid[src.empty_at[0]][src.empty_at[1]],cgrid[src.empty_at[0]+s[0]][src.empty_at[1]+s[1]]            
 
-                child = Grid(cgrid,g.empty_at)
-                print(child.calculate_fitness())
-                # print(child.fitness)
-                # child.fitness = child.calculate_fitness()
-                # print(child.fitness)
-
-                # output(child)
-                
-                queue.append(child)
+                    child = Grid(cgrid,src.empty_at)
+                    child.calculate_fitness()
+    
+                    child.calculate_fitness()
+        
+                    output(child)
+                    
+                    if (child.grid == goal):
+                        print("***************************")
+                        output(child.grid)
+                        return 0 
+                    
+                    next_queue.append(child)
             pass 
-        break
-        pass   
+        # break
+        output(src) 
 
 if __name__ == "__main__":
     initialize()
     print(distance)
     
     g = Grid([
-        [2,1,3],
-        [8,0,4],
-        [7,6,5],
+        [2,8,3],
+        [6,5,1],
+        [0,7,4],
     ])
 
-    print(g.calculate_fitness())
-    print(g.fitness)
+    g.calculate_fitness()
+    
 
-    output(g)
     BFS(g)
     # get_nabors(g)
-    output(g)
+    # output(g)
+    # output(h)
+    # output(u)
 
-    a =1
-    b= 2
-    a,b = b,a
-
-    print(a,b)
-    # print(cgrid)
+   
